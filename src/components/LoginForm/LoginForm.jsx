c2a5aebba93b8bb8b2fadef4b336be74ba0b7ead
NadiiaUa23 <hasiminka92@gmail.com>
2024-08-06 21:02:01 +0200

    неконтрольвана форма

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,15 +1,23 @@
-// import style from './LoginForm.module.css'
+import style from './LoginForm.module.css'
 const LoginForm = () => {
 const handleSubmit = (evt) => {
-    
+    evt.preventDefault();
+
+
+const form = evt.target;
+const {login, password} = form.elements;
+
+console.log(login, password);
+console.log(login.value, password.value);
+
+form.reset();
 }
 
-
     return (
-      <form>
-        <input type="text" name="login" />
-        <input type="password" name="password" />
-        <button type="submit">Log in</button>
+      <form onSubmit={handleSubmit} className={style.container}>
+        <input type="text" name="login"  placeholder="Username" className={style.input}/>
+        <input type="password" name="password"   placeholder="Password" className={style.input}/>
+        <button type="submit" className={style.button}>Log in</button>
       </form>
     );
   };
