655ad2ecfc9dc2f5177b4d8c1c43eee4006b1798
NadiiaUa23 <hasiminka92@gmail.com>
2024-08-16 19:32:10 +0200

    компонент SearchBar в React, который использует хуки для управления состоянием компонента.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-inport style from './SearchBar.modules.css';
+// inport style from './SearchBar.modules.css';
 import { useState } from "react";
 
 const SearchBar = () => {
@@ -18,3 +18,8 @@ const SearchBar = () => {
   
 
 export  default SearchBar;
+//Отримуємо замкнене коло.
+// Користувач вводить символ в поле.
+// Викликається колбек-функція, передана в onChange.
+// Під час події onChange оновлюється значення стану.
+// Під час зміни стану відбувається повторний рендер поля, оскільки значення атрибута value залежить від стану.//
