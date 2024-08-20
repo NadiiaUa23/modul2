8d59625ceb0a2fe51b797a37851140710d7faa86
NadiiaUa23 <hasiminka92@gmail.com>
2024-08-20 21:42:00 +0200

    Якщо селект використовується поза формою, то ми працюємо з ним як з контрольованим елементом:
    Зберігаємо поточне значення в стані.
    Передаємо значення стану як атрибут value.
    Змінюємо стан при події onChange.

--- src/components/LangSwitcher/LangSwitcher.jsx
+++ src/components/LangSwitcher/LangSwitcher.jsx
@@ -1,17 +1,37 @@
 import style from './LangSwitcher.module.css'
-import {useId } from "react";
+import {useId, useState } from "react";
 
 
+// const LangSwitcher = () => {
+//     const selectId = useId();
+  
+//     return (
+//         /*{Выпадающий список (select)}*/
+//       <div className={style.conteiner} >
+//         <label htmlFor={selectId}>Choose language</label>
+//         <select id={selectId}>
+//           <option value="en">English</option>
+//           <option value="uk">Ukrainian</option>
+//           <option value="pl">Polish</option>
+//         </select>
+//       </div>
+//     );
+//   };
+  
 const LangSwitcher = () => {
     const selectId = useId();
+    const [lang, setLang] = useState("uk");
   
     return (
-        /*{Выпадающий список (select)}*/
-      <div className={style.conteiner} >
+      <div className={style.conteiner}>
         <label htmlFor={selectId}>Choose language</label>
-        <select id={selectId}>
-          <option value="en">English</option>
+        <select
+          id={selectId}
+          value={lang}
+          onChange={(evt) => setLang(evt.target.value)}
+        >
           <option value="uk">Ukrainian</option>
+          <option value="en">English</option>
           <option value="pl">Polish</option>
         </select>
       </div>
