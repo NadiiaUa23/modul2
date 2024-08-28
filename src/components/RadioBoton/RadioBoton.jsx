428ab0671dbb49f62d434ee743aea088fe3309d2
NadiiaUa23 <hasiminka92@gmail.com>
2024-08-28 22:18:50 +0200

    передали атрибут onChange, щоб при виборі опції змінювати стан. Передамо кожній радіо-кнопці в групі атрибут onChange, значенням якого буде функція, всередині якої ми записуємо обране значення в стан компонента.

--- src/components/RadioBoton/RadioBoton.jsx
+++ src/components/RadioBoton/RadioBoton.jsx
@@ -3,40 +3,46 @@ import { useState } from "react";
 
 const RadioBoton = () => {
     const [coffeeSize, setCoffeeSize] = useState("sm");
-    
-    return (
-      <>
-        <h1>Select coffee size</h1>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="sm"
-            checked={coffeeSize === "sm"}
-          />
-          Small
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="md"
-            checked={coffeeSize === "md"}
-          />
-          Meduim
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="lg"
-            checked={coffeeSize === "lg"}
-          />
-          Large
-        </label>
-      </>
-    );
-  };
+    const handleSizeChange = (evt) => {
+        setCoffeeSize(evt.target.value);
+      };
+
+    return  (
+        <>
+          <h1>Select coffee size</h1>
+          <label>
+            <input
+              type="radio"
+              name="coffeeSize"
+              value="sm"
+              checked={coffeeSize === "sm"}
+              onChange={handleSizeChange}
+            />
+            Small
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="coffeeSize"
+              value="md"
+              checked={coffeeSize === "md"}
+              onChange={handleSizeChange}
+            />
+            Meduim
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="coffeeSize"
+              value="lg"
+              checked={coffeeSize === "lg"}
+              onChange={handleSizeChange}
+            />
+            Large
+          </label>
+        </>
+      );
+    };
   
 
 
