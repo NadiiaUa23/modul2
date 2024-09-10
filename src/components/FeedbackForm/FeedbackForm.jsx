3434da540d76bf7ed126cf71b302856b4861e558
NadiiaUa23 <hasiminka92@gmail.com>
2024-09-10 19:32:15 +0200

    додаемо селект
     Field as="select"
    option value="good">Good</option>

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -5,13 +5,15 @@ import css from './FeedbackForm.module.css'
 const initialValues = {
     username: "",
     email: "",
-    message: ""
+    message: "",
+    level: "good",
 };
 
 const FeedbackForm = () => {
     const nameFieldId = useId();
     const emailFieldId = useId();
     const msgFieldId = useId();
+    const levelFieldId = useId();
 
     const handleSubmit = (values, actions) => {
 		console.log(values);
@@ -26,9 +28,17 @@ const FeedbackForm = () => {
     <Field  className={css.field} type="text" name="username"  id={nameFieldId} />
     <label htmlFor={emailFieldId} className={css.label} >Email: </label>
     <Field  className={css.field} type="email" name="email" id={emailFieldId}/>
-    <label htmlFor={msgFieldId}>Message</label>
-        <Field as="textarea" name="message" id={msgFieldId} rows="5" />
-	 <button  classname={css.btn} type="submit">Submit</button>
+    <label htmlFor={msgFieldId} className={css.label} >Message</label>
+        <Field as="textarea" name="message" id={msgFieldId} rows="5" className={css.field} />
+	 
+        <label htmlFor={levelFieldId}>Service satisfaction level</label>
+        <Field as="select" name="level" id={levelFieldId}>
+          <option value="good">Good</option>
+          <option value="neutral">Neutral</option>
+          <option value="bad">Bad</option>
+        </Field>
+
+     <button  classname={css.btn} type="submit">Submit</button>
      </Form>
     </Formik>
 
