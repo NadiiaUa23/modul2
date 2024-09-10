ad69c24acce874fa7ed3540b85a5747ac6c2b367
NadiiaUa23 <hasiminka92@gmail.com>
2024-09-10 18:09:26 +0200

    Типи Додамо до форми багаторядкове поле вводу для тексту відгуку.
    Додаємо поле компонентом Field з пропсом as="textarea"
    Додаємо до об'єкта початкових значень нову властивість
    Використовуємо useId для отримання ідентифікатораполів

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -4,12 +4,14 @@ import css from './FeedbackForm.module.css'
 
 const initialValues = {
     username: "",
-    email: ""
+    email: "",
+    message: ""
 };
 
 const FeedbackForm = () => {
     const nameFieldId = useId();
     const emailFieldId = useId();
+    const msgFieldId = useId();
 
     const handleSubmit = (values, actions) => {
 		console.log(values);
@@ -24,8 +26,10 @@ const FeedbackForm = () => {
     <Field  className={css.field} type="text" name="username"  id={nameFieldId} />
     <label htmlFor={emailFieldId} className={css.label} >Email: </label>
     <Field  className={css.field} type="email" name="email" id={emailFieldId}/>
-    <button  classname={css.btn} type="submit">Submit</button>
-	</Form>
+    <label htmlFor={msgFieldId}>Message</label>
+        <Field as="textarea" name="message" id={msgFieldId} rows="5" />
+	 <button  classname={css.btn} type="submit">Submit</button>
+     </Form>
     </Formik>
 
 );
